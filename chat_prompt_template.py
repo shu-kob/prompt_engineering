3d5964979b6d470ca1b64e08148c4af2f352fcaa
小渕周 <shukobuchi@gmail.com>
2024-12-17 21:00:28 +0900

    system_promptを変数化

--- chat_prompt_template.py
+++ chat_prompt_template.py
@@ -1,18 +1,23 @@
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_google_vertexai import VertexAI
 
-llm = VertexAI(model_name="gemini-1.5-flash-002")
+llm = VertexAI(model_name="gemini-2.0-flash-exp")
 
 prompt_template = ChatPromptTemplate.from_messages([
-    ("system", "ステップバイステップで考えてください。"),
+    ("system", "{system_prompt}"),
     ("human", "{question}"),
 ])
 
 chain = prompt_template | llm
 
 question = """
-10 + 2 * 3 - 4 * 2
+生成AIの仕組みを教えてください。
 """
-print(chain.invoke({"question": question}))
+
+system_prompt = """
+ITコンサルタントになったように説明してください。
+"""
+
+print(chain.invoke({"system_prompt": system_prompt, "question": question}))
 
 # pip install pydantic==2.9.0
